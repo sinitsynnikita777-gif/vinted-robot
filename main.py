a9fef87d56fbaa9ef7cd70cc93fb73dcbfdcbab8
sinitsynnikita777-gif <sinitsynnikita777@gmail.com>
2026-04-25 07:43:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import requests
 import time
 
+URL = "https://www.vinted.co.uk/api/v2/catalog/items"
+
+PARAMS = {
+    "page": 1,
+    "per_page": 10,
+    "order": "newest_first"
+}
+
 while True:
-    print("bot running...")
-    time.sleep(10)
+    try:
+        r = requests.get(URL, params=PARAMS)
+        data = r.json()
+
+        items = data.get("items", [])
+
+        for item in items:
+            print(item["title"], item["price"], item["url"])
+
+        print("-----")
+        time.sleep(30)
+
+    except Exception as e:
+        print("error:", e)
+        time.sleep(10)
